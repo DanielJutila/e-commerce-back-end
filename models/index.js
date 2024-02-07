2fcd857afee09b49aaf570826a91413c28942237
Daniel Jutila <daniel.jutila@gmail.com>
2024-02-07 12:21:31 -0600

    index and some models are done. Halfway there.

--- models/index.js
+++ models/index.js
@@ -5,13 +5,37 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  through: {
+    model: Category,
+    unique: false
+  },
+  as: 'product_category'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  through: {
+    model: Product,
+    unique: false
+  },
+  as: 'category_product'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, { 
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 module.exports = {
   Product,
   Category,
